86f601227a85caa052d403389c3452fb458a2b5e
AIsmaellerena <129919789+AIsmaellerena@users.noreply.github.com>
2024-12-10 10:08:09 -0500

    Update Main.java

--- InventarioHotel/src/InventarioHotel/Main.java
+++ InventarioHotel/src/InventarioHotel/Main.java
@@ -4,22 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Inventario inventario = new Inventario();
 
-        // Crear productos
         Producto p1 = new Producto("Agua", 50, 0.50);
         Producto p2 = new Producto("Coca Cola", 30, 1.00);
 
-        // Agregar productos al inventario
         inventario.agregarProducto(p1);
         inventario.agregarProducto(p2);
 
-        // Mostrar inventario
         inventario.mostrarInventario();
 
-        // Registrar una compra
         Compra compra = new Compra(p1, 20);
         System.out.println(compra);
 
-        // Registrar una venta
         try {
             Venta venta = new Venta(p2, 10);
             System.out.println(venta);
@@ -27,7 +22,6 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        // Mostrar inventario actualizado
         inventario.mostrarInventario();
     }
 }
